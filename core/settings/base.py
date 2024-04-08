bb19ca3a8445c02e88e736796616157b5493da3b
Jamshid Boltaboyev <j.boltaboyev@uic.group>
2024-04-08 09:29:19 +0500

    fix: ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS

--- core/settings/base.py
+++ core/settings/base.py
@@ -23,7 +23,11 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", 'localhost', "afsona.transitgroup.uz"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://afsona.transitgroup.uz",
+    "https://afsona.transitgroup.uz"
+]
 
 # Application definition
 DJANGO_APPS = [
