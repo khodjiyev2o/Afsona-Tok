824ad7ca6d4cb76221794b1794119b30a7ff5ce9
Jamshid Boltaboyev <j.boltaboyev@uic.group>
2024-04-11 18:50:42 +0500

    feat: configure mobile ws

--- apps/chargers/consumers.py
+++ apps/chargers/consumers.py
@@ -1,12 +1,25 @@
-from channels.generic.websocket import WebsocketConsumer
+import json
+
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
-class EchoConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class EchoConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        await self.channel_layer.group_add('connectors', self.channel_name)
+        await self.accept()
+        print("Connected to WebSocket and added to 'connectors' group")
 
-    def disconnect(self, close_code):
-        pass
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard('connectors', self.channel_name)
+        print("Disconnected from WebSocket and removed from 'connectors' group")
 
-    def receive(self, text_data=None, bytes_data=None):
-        self.send(text_data=text_data)
+    async def connector_status(self, event):
+        status = event['status']
+        connector_id = event['connector_id']
+        await self.send(
+            text_data=json.dumps({
+            'type': "connector":{
+                'connector_id': connector_id
+                'status': status
+            }
+        }))
