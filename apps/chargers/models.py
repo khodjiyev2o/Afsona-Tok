3ef0840581b0834f76f80434297c08372543fa92
Jamshid Boltaboyev <j.boltaboyev@uic.group>
2024-04-15 23:25:38 +0500

    feat: comment charging transaction save method

--- apps/chargers/models.py
+++ apps/chargers/models.py
@@ -129,10 +129,10 @@ class ChargingTransaction(BaseModel):
     def consumed_kwh(self) -> float:
         return round((self.meter_on_start - self.meter_on_end if self.meter_on_end else 0) / 1000, 2)
 
-    def save(self, *args, **kwargs):
-        if not self.battery_percent_on_start:
-            self.battery_percent_on_start = self.battery_percent_on_end
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     if not self.battery_percent_on_start:
+    #         self.battery_percent_on_start = self.battery_percent_on_end
+    #     super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.id}: {self.user} - {self.total_price}"
