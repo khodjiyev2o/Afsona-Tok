09b48ac10f1bfb3965559768d668ae6a1891b762
s.khodjiyev@uic.group <samandarkhodjiyev@gmail.com>
2024-04-20 16:15:55 +0500

    feaT: refersh token

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from apps.users.api_endpoints import *
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path("login/confirm/", LoginConfirmView.as_view(), name='login_confirm'),
     path("profile-update/", ProfileUpdateView.as_view(), name='profile_update'),
     path("profile-detail/", ProfileDetailView.as_view(), name='profile_detail'),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
