ee716bf0726a96efdf3f3d010ac7a82ed0c752a0
Jamshid Boltaboyev <j.boltaboyev@uic.group>
2024-04-25 00:08:50 +0500

    fix: add description to UserNotificationList

--- apps/notification/api_endpoints/UserNotificationDetail/serializers.py
+++ apps/notification/api_endpoints/UserNotificationDetail/serializers.py
@@ -3,9 +3,6 @@ from apps.notification.models import UserNotification
 
 
 class UserNotificationDetailSerializer(serializers.ModelSerializer):
-    title = serializers.CharField(source='notification.title')
-    description = serializers.CharField(source='notification.description')
-
     class Meta:
         model = UserNotification
-        fields = ('id', 'title', 'description', 'is_read', 'created_at')
+        fields = ('id', 'is_read')
