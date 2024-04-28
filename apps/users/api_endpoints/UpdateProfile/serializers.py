fca632866c197949f82edffba1ec25c45c2c3492
s.khodjiyev@uic.group <samandarkhodjiyev@gmail.com>
2024-04-28 11:57:02 +0500

    fix: update profile

--- apps/users/api_endpoints/UpdateProfile/serializers.py
+++ apps/users/api_endpoints/UpdateProfile/serializers.py
@@ -7,3 +7,4 @@ class ProfileUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("full_name", "date_of_birth", "photo", "language")
+        read_only_fields = ("phone", "balance")
