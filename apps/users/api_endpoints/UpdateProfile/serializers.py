f7cd9c1e42501ebf9a7c143e67e7cf297663928c
s.khodjiyev@uic.group <samandarkhodjiyev@gmail.com>
2024-04-28 12:04:00 +0500

    fix: update profile

--- apps/users/api_endpoints/UpdateProfile/serializers.py
+++ apps/users/api_endpoints/UpdateProfile/serializers.py
@@ -6,5 +6,5 @@ from apps.users.models import User
 class ProfileUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("full_name", "date_of_birth", "photo", "language")
+        fields = ("full_name", "date_of_birth", "photo", "language", "phone", "balance")
         extra_kwargs = {"phone": {"read_only": True}, "balance": {"read_only": True}}
