a736cc53cdda1c28fff3c3f52a66e5507ddcad6c
s.khodjiyev@uic.group <samandarkhodjiyev@gmail.com>
2024-04-28 12:24:22 +0500

    fix: update profile

--- apps/users/api_endpoints/UpdateProfile/serializers.py
+++ apps/users/api_endpoints/UpdateProfile/serializers.py
@@ -4,9 +4,7 @@ from apps.users.models import User
 
 
 class ProfileUpdateSerializer(serializers.ModelSerializer):
-    phone = serializers.ReadOnlyField()
-    balance = serializers.ReadOnlyField()
 
     class Meta:
         model = User
-        fields = ("full_name", "date_of_birth", "photo", "language", "phone", "balance")
+        fields = ("full_name", "date_of_birth", "photo", "language")
