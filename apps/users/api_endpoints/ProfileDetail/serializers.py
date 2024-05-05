ae713fb8f43090e8e47c0ce56559735d2189e744
s.khodjiyev@uic.group <samandarkhodjiyev@gmail.com>
2024-05-05 11:59:00 +0500

    fix: notification count

--- apps/users/api_endpoints/ProfileDetail/serializers.py
+++ apps/users/api_endpoints/ProfileDetail/serializers.py
@@ -4,10 +4,12 @@ from apps.notification.models import UserNotification
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    notification_cout = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = ("full_name", "photo", "phone", "balance", "date_of_birth", "language", "notification_count")
 
-    def notification_count(self, obj):
+    def get_notification_count(self, obj):
         return UserNotification.objects.filter(user=obj, is_read=False).count()
 
