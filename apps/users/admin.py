2823a6490896f0bc09428aec1ce8f11b1bf817b3
Jamshid Boltaboyev <j.boltaboyev@uic.group>
2024-05-05 19:12:57 +0500

    feat: Implement Postgres View for show all transactions

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
+from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+from .models import User, AllTransactionHistory
+from apps.chargers.proxy_models import FinishedChargingTransactionProxy
+from apps.payment.models import Transaction
+from django.urls import reverse_lazy
 
 
 @admin.register(User)
@@ -21,7 +26,7 @@ class UserAdmin(BaseUserAdmin):
                 ),
             },
         ),
-       )
+    )
     add_fieldsets = (
         (
             None,
@@ -42,3 +47,35 @@ class UserAdmin(BaseUserAdmin):
     readonly_fields = ("id", "created_at", "balance")
     exclude = ("last_login", "updated_at", "password", "is_active", "email")
     ordering = ("-created_at",)
+
+
+@admin.register(AllTransactionHistory)
+class AllTransactionHistoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'amount', 'action', "created_at", "get_more_information"]
+    list_filter = ['created_at', 'user', 'action']
+    autocomplete_fields = ['user']
+    search_fields = ['user__phone', 'action']
+    date_hierarchy = 'created_at'
+
+    def get_more_information(self, obj: AllTransactionHistory):
+        model_mapping = {
+            AllTransactionHistory.Actions.CHARGE.value: FinishedChargingTransactionProxy,
+            AllTransactionHistory.Actions.PAYMENT.value: Transaction,
+        }
+        model = model_mapping.get(obj.action, None)
+        if not model:
+            return _("No more information available")
+
+        url = reverse_lazy(f"admin:{model._meta.app_label}_{model._meta.model_name}_change", args=[obj.id])
+        return mark_safe(f'<a target="_blank" href="{url}">{_("More info")}</a>')
+
+    get_more_information.short_description = _("More info")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
