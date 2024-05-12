b402123c3eb8eb16e79ecd6de76f02d3447e380c
s.khodjiyev@uic.group <samandarkhodjiyev@gmail.com>
2024-05-12 15:15:28 +0500

    feat: formula closenes

--- apps/chargers/api_endpoints/LocationList/views.py
+++ apps/chargers/api_endpoints/LocationList/views.py
@@ -1,28 +1,47 @@
-from django.db.models import Exists
+from math import radians, sin, cos, sqrt, atan2
+from decimal import Decimal
+from django.db.models import F
+
 from rest_framework import generics
 
 from apps.chargers.models import Location
-from apps.common.models import SavedLocation
-
 from apps.chargers.api_endpoints.LocationList.serializers import LocationListSerializer
 
 
 class LocationListView(generics.ListAPIView):
-    """List of locations, send user_latitude and user_longitude
-    as query parameters to get distance from user location"""
     serializer_class = LocationListSerializer
     search_fields = ('name', 'address', 'district__name')
 
     def get_queryset(self):
-        return Location.objects.select_related('district').prefetch_related(
-            'chargers', 'chargers__connectors')
+        user_latitude = Decimal(self.request.query_params.get('user_latitude'))
+        user_longitude = Decimal(self.request.query_params.get('user_longitude'))
+
+        queryset = Location.objects.annotate(
+            distance=self.calculate_distance(user_latitude, user_longitude, F('latitude'), F('longitude'))
+        ).order_by('distance')
+
+        return queryset
+
+    def calculate_distance(self, lat1, lon1, lat2, lon2):
+        # Haversine formula to calculate distance
+        lat1_rad, lon1_rad = radians(lat1), radians(lon1)
+        lat2_rad, lon2_rad = radians(lat2), radians(lon2)
+
+        dlon = lon2_rad - lon1_rad
+        dlat = lat2_rad - lat1_rad
+
+        a = sin(dlat / 2) ** 2 + cos(lat1_rad) * cos(lat2_rad) * sin(dlon / 2) ** 2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+        radius_earth = 6371  # Radius of the Earth in kilometers
+        distance_km = radius_earth * c
+
+        return distance_km
 
     def get_serializer_context(self):
-        # Get latitude and longitude from request parameters
         latitude = self.request.query_params.get('user_latitude')
         longitude = self.request.query_params.get('user_longitude')
 
-        # Create a context dictionary with latitude and longitude
         context = super().get_serializer_context()
         context['user_latitude'] = latitude
         context['user_longitude'] = longitude
