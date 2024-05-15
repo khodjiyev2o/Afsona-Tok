0b4b73ba83e17a2d59df7820d42eaff99af3c4cd
Jamshid Boltaboyev <j.boltaboyev@uic.group>
2024-05-15 06:50:40 +0500

    feat: add diagnostics_file_upload url and view

--- apps/chargers/ocpp_messages/urls.py
+++ apps/chargers/ocpp_messages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 
 from apps.chargers.ocpp_messages.views import *
+from apps.chargers.ocpp_messages.views.diagnostics import DiagnosticsFileUploadView
 
 app_name = 'ocpp_messages'
 
@@ -13,5 +14,7 @@ urlpatterns = [
     path("<str:charger_identify>/status_notification/", StatusNotificationAPIView.as_view()),
     path("<str:charger_identify>/stop_transaction/", StopTransactionAPIView.as_view()),
     path("<str:charger_identify>/disconnect/", ChargerDisconnectAPIView.as_view()),
-    path("command-callback/<str:id_tag>/", CommandCallbackAPIView.as_view())
+    path("command-callback/<str:id_tag>/", CommandCallbackAPIView.as_view()),
+
+    path('diagnostics_file_upload/', DiagnosticsFileUploadView.as_view()),
 ]
---
+++ apps/chargers/ocpp_messages/views/diagnostics.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+
+
+class DiagnosticsFileUploadView(View):
+
+    @csrf_exempt
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if request.FILES:
+            uploaded_file = request.FILES['file']
+            file_path = settings.MEDIA_ROOT.joinpath(uploaded_file.name)
+            with open(file_path, 'wb+') as destination:
+                for chunk in uploaded_file.chunks():
+                    destination.write(chunk)
+
+            return HttpResponse(status=200)
+        else:
+            return HttpResponse(status=400)
