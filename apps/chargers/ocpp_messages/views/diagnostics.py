f01394396531654bb747a81b8ca53f95ab1102fe
Jamshid Boltaboyev <j.boltaboyev@uic.group>
2024-05-15 07:03:11 +0500

    fix: add diagnostics_file_upload url and view

--- apps/chargers/ocpp_messages/views/diagnostics.py
+++ apps/chargers/ocpp_messages/views/diagnostics.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
-
+from django.utils import timezone
 
 class DiagnosticsFileUploadView(View):
 
@@ -13,7 +13,7 @@ class DiagnosticsFileUploadView(View):
     def post(self, request, *args, **kwargs):
         if request.FILES:
             uploaded_file = request.FILES['file']
-            file_path = settings.MEDIA_ROOT.joinpath(uploaded_file.name)
+            file_path = settings.MEDIA_ROOT.joinpath(f"{timezone.now()}{uploaded_file.name}")
             with open(file_path, 'wb+') as destination:
                 for chunk in uploaded_file.chunks():
                     destination.write(chunk)
