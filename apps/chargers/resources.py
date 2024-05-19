3d7f9238b606989f7fba89f2e828ea1a973872ed
s.khodjiyev@uic.group <samandarkhodjiyev@gmail.com>
2024-05-19 12:59:53 +0500

    feat: added cash mode to excel

--- apps/chargers/resources.py
+++ apps/chargers/resources.py
@@ -1,15 +1,20 @@
 from django.utils.translation import gettext_lazy as _
-from import_export import resources
+from import_export import resources, fields
 
 from apps.chargers.proxy_models import FinishedChargingTransactionProxy
 
 
 class FinishedChargingTransactionProxyResource(resources.ModelResource):
+    cash_mode = fields.Field(column_name=_("Cash Mode"))
+
     class Meta:
         model = FinishedChargingTransactionProxy
-        export_order = ('id', "connector__charge_point__name", 'created_at', 'meter_used', 'total_price')
-        fields = ('id', 'connector__charge_point__name', 'created_at', 'meter_used', 'total_price')
+        export_order = ('id', "connector__charge_point__name", 'created_at', 'cash_mode', 'meter_used', 'total_price')
+        fields = ('id', 'connector__charge_point__name', 'created_at', 'cash_mode', 'meter_used', 'total_price',)
         name = _("Export Charging Transactions")
 
+    def dehydrate_cash_mode(self, obj):
+        return obj.user is None
+
     def get_export_headers(self, fields=None):
-        return [_("ID"), _("Charger Name"), _("Created At"), _("Meter Used"), _("Total Price")]
+        return [_("ID"), _("Charger Name"), _("Created At"),  _("Cash Mode"), _("Meter Used"), _("Total Price")]
