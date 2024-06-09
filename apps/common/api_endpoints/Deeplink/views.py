600e4e58f1c418ea39c4dd5b16edd1b186f78028
Jamshid Boltaboyev <j.boltaboyev@uic.group>
2024-06-09 10:47:05 +0500

    fix: auto redirect to app store or play store

--- apps/common/api_endpoints/Deeplink/views.py
+++ apps/common/api_endpoints/Deeplink/views.py
@@ -1,8 +1,23 @@
 from django.views.generic import TemplateView
+from django_user_agents.utils import get_user_agent
+from user_agents.parsers import UserAgent
+
+from django.shortcuts import redirect
 
 
 class AppRedirectAPIView(TemplateView):
     template_name = "deeplink.html"
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        django_user_agent: UserAgent = get_user_agent(request)
+
+        if django_user_agent.os.family == 'iOS':
+            return redirect(to=context['apple_store_link'])
+        elif django_user_agent.os.family == 'Android':
+            return redirect(to=context['apple_store_link'])
+
+        return self.render_to_response(context)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
