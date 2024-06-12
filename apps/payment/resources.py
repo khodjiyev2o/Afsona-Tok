9b30321f1af39d5beb13fc430de4059bd5645706
Jamshid Boltaboyev <jamshidbek.boltaboyev.2004@gmail.com>
2024-06-12 22:20:25 +0500

    fix: PaymentTransactionResource's card__card_number field

--- apps/payment/resources.py
+++ apps/payment/resources.py
@@ -5,24 +5,16 @@ from apps.payment.models import Transaction as PaymentTransaction
 
 
 class PaymentTransactionResource(resources.ModelResource):
-    card__card_number = fields.Field(
-        attribute='get_card_number', readonly=True,
-        widget=widgets.CharWidget()
-    )
+    card_number = fields.Field(readonly=True, widget=widgets.CharWidget())
 
     class Meta:
         model = PaymentTransaction
-        export_order = ('id', 'card__card_number', 'user__phone', 'amount', 'payment_type', 'created_at', 'status')
-        fields = ('id', 'card__card_number', 'user__phone', 'amount', 'payment_type', 'created_at', 'status')
+        export_order = ('id', 'card_number', 'user__phone', 'amount', 'payment_type', 'created_at', 'status')
+        fields = ('id', 'card_number', 'user__phone', 'amount', 'payment_type', 'created_at', 'status')
         name = _("Export Payment Transactions")
+
+    def dehydrate_card_number(self, obj: PaymentTransaction):
+        return obj.card.card_number if obj.card else ""
 
     def get_export_headers(self, fields=None):
         return [_("ID"), _("Card Number"), _("Client"), _("Amount"), _("Payment Type"), _("Created At"), _("Status")]
-
-    def get_card_number(self, obj: PaymentTransaction):
-        return obj.card.card_number
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        queryset = queryset.select_related('card')
-        return queryset
