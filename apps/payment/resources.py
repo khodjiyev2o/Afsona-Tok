a08f3aa784d52df776f705769a79875cf457d40d
Jamshid <admin@MacBook-Pro-admin.local>
2024-06-12 21:12:05 +0500

    fix: PaymentTransactionResource's card__card_number field

--- apps/payment/resources.py
+++ apps/payment/resources.py
@@ -1,11 +1,15 @@
 from django.utils.translation import gettext_lazy as _
-from import_export import resources
+from import_export import resources, fields, widgets
 
 from apps.payment.models import Transaction as PaymentTransaction
 
 
 class PaymentTransactionResource(resources.ModelResource):
-    
+    card__card_number = fields.Field(
+        attribute='get_card_number', readonly=True,
+        widget=widgets.CharWidget()
+    )
+
     class Meta:
         model = PaymentTransaction
         export_order = ('id', 'card', 'user__phone', 'amount', 'payment_type', 'created_at', 'status')
@@ -14,3 +18,11 @@ class PaymentTransactionResource(resources.ModelResource):
 
     def get_export_headers(self, fields=None):
         return [_("ID"), _("Card Number"), _("Client"), _("Amount"), _("Payment Type"), _("Created At"), _("Status")]
+
+    def get_card_number(self, obj: PaymentTransaction):
+        return obj.card.card_number
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.select_related('card')
+        return queryset
