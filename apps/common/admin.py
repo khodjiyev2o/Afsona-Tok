23d709a20854660d392404664a3bb2e495eee983
Samandar <92217667+khodjiyev2o@users.noreply.github.com>
2024-06-18 11:22:10 +0500

    Update admin.py

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -183,8 +183,5 @@ class SavedLocationAdmin(admin.ModelAdmin):
     def has_change_permission(self, request, obj=None):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
-
     def has_add_permission(self, request):
         return False
